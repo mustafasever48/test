d36cff0de0fbb4bd8d58c06254cb66a5966cae0f
mustafasever48 <82729984+mustafasever48@users.noreply.github.com>
2023-11-30 14:25:24 +0000

    replace('\n',' ')

--- app.py
+++ app.py
@@ -67,7 +67,7 @@ def hello(): # Name of the method
   Results=[]
   for row in rv: #Format the Output Results and add to return string
     Result={}
-    Result['Brand_Name']=row[1]
+    Result['Brand_Name']=row[1].replace('\n',' ')
     Result['Brand_ID ']=row[0]
 
 
