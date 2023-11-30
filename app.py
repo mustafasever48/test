9a0f2ef7cfeb3e7473186dc18cac07d1538db38c
mustafasever48 <82729984+mustafasever48@users.noreply.github.com>
2023-11-30 13:13:23 +0000

    cur.execute('''SELECT * FROM Product''')  # execute an SQL statment

--- app.py
+++ app.py
@@ -35,63 +35,58 @@ app = Flask(__name__)
 CORS(app)
 
 
-
 @app.route("/add", methods=['GET', 'POST'])
-
 def add():
-  if request.method == 'POST':
-    brandName = request.form['Brand_Name']
-    modelName = request.form['Mode_Name']
-    productName = request.form['Product_Name']
-    serialNumber = request.form['Serial_Number']
-    print(brandName,modelName,productName,serialNumber)
+    if request.method == 'POST':
+        brandName = request.form['Brand_Name']
+        modelName = request.form['Mode_Name']
+        productName = request.form['Product_Name']
+        serialNumber = request.form['Serial_Number']
+        print(brandName, modelName, productName, serialNumber)
+        cur = mysql.cursor()
+
+        s_brand = '''INSERT INTO Brand(Brand_Name) VALUES('{}');'''.format(brandName)
+        cur.execute(s_brand)
+        s_model = '''INSERT INTO Model(Model_Name) VALUES('{}');'''.format(modelName)
+        cur.execute(s_model)
+        s_product = '''INSERT INTO Product(Product_Name, Serial_Number) VALUES('{}', '{}');'''.format(productName, serialNumber)
+        cur.execute(s_product)
+        mysql.commit()
+    else:
+        return render_template('add.html')
+
+    return '{"Result":"Success"}'
+
+
+@app.route("/")  # Default - Show Data
+def hello():
     cur = mysql.cursor()
-    
-    s_brand = '''INSERT INTO Brand(Brand_Name) VALUES('{}');'''.format(brandName)
-    cur.execute(s_brand)
-    s_model = '''INSERT INTO Model(Model_Name) VALUES('{}');'''.format(modelName)
-    cur.execute(s_model)
-    mysql.commit()
-    s_product = '''INSERT INTO Product(Product_Name, Serial_Number) VALUES('{}', {});'''.format(productName, serialNumber)
-    cur.execute(s_product, (product_name, serial_number))
-    mysql.commit()
-  else:
-    return render_template('add.html')
-    
-  return '{"Result":"Success"}'
-    
-    
 
+    cur.execute('''SELECT * FROM Brand''')  # execute an SQL statment
+    brand_results = cur.fetchall()
 
+    cur.execute('''SELECT * FROM Model''')  # execute an SQL statment
+    model_results = cur.fetchall()
 
+    cur.execute('''SELECT * FROM Product''')  # execute an SQL statment
+    product_results = cur.fetchall()
 
-@app.route("/") #Default - Show Data
-def hello(): # Name of the method
-  cur = mysql.cursor() #create a connection to the SQL instance
-  
-  cur.execute('''SELECT * FROM Brand''') # execute an SQL statment
-  cur.execute('''SELECT * FROM Model''') # execute an SQL statment
-  cur.execute('''SELECT * FROM Product''') # execute an SQL statment
-  
-  rv = cur.fetchall() #Retreive all rows returend by the SQL statment
-  Results=[]
-  for row in rv: #Format the Output Results and add to return string
-    Result={}
-    Result['Brand']=row[0].replace('\n',' ')
-    Result['Model']=row[1]
-    Result['Product']=row[2]
-    Result['Serial Number']=row[3]
-    Results.append(Result)
-  response={'Results':Results, 'count':len(Results)}
-  ret=app.response_class(
-    response=json.dumps(response),
-    status=200,
-    mimetype='application/json'
-  )
-  return ret #Return the data in a string format
+    Results = {
+        'Brand': brand_results,
+        'Model': model_results,
+        'Product': product_results
+    }
 
-
+    response = {'Results': Results, 'count': len(brand_results)}
+    ret = app.response_class(
+        response=json.dumps(response),
+        status=200,
+        mimetype='application/json'
+    )
+    return ret  # Return the data in a string format
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port='8080', debug=True, ssl_context=('/etc/letsencrypt/live/msubuntu.northeurope.cloudapp.azure.com/cert.pem', '/etc/letsencrypt/live/msubuntu.northeurope.cloudapp.azure.com/privkey.pem'))
+    app.run(host='0.0.0.0', port='8080', debug=True,
+            ssl_context=('/etc/letsencrypt/live/msubuntu.northeurope.cloudapp.azure.com/cert.pem',
+                          '/etc/letsencrypt/live/msubuntu.northeurope.cloudapp.azure.com/privkey.pem'))
