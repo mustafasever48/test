e102322646a699705ceceabe5d2ba770cb3aa89a
mustafasever48 <82729984+mustafasever48@users.noreply.github.com>
2023-11-30 20:48:55 +0000

    remove warrant check

--- app.py
+++ app.py
@@ -75,8 +75,6 @@ def hello():
         Result['Product_Name'] = row[2]
         Result['Serial_Number'] = row[3]
         Result['ProductSoldDate'] = row[4].isoformat() if row[4] else None
-        warranty_check = (datetime.now() - row[4]).days
-        Result['WarrantyCheck'] = 'Warranty is still valid.' if warranty_check <= 730 else 'Warranty has expired.'
         Results.append(Result)
         
 
