5e85e154e0af0748d00dcc747c9220e1193372ba
mustafasever48 <82729984+mustafasever48@users.noreply.github.com>
2023-12-05 12:03:15 +0000

    end

--- app.py
+++ app.py
@@ -3,10 +3,7 @@ import mysql.connector
 from flask_cors import CORS
 import json
 from datetime import datetime
-
-app = Flask(__name__)
-CORS(app)
-CORS(app, resources={r"/*": {"origins": "https://msubuntu.northeurope.cloudapp.azure.com"}})
+from datetime import datetime, timedelta
 
 mysql = mysql.connector.connect(
     user='web',
@@ -15,37 +12,42 @@ mysql = mysql.connector.connect(
     database='rma'
 )
 
+app = Flask(__name__)
+CORS(app)
+
 @app.route("/add", methods=['POST'])
 def add():
     if request.method == 'POST':
-        brand_name = request.form['Brand_Name']
-        model_name = request.form['Model_Name']
-        product_name = request.form['Product_Name']
-        serial_number = request.form['Serial_Number']
-        product_sold_date = request.form['Product_Sold_Date']
+        brandName = request.form['Brand_Name']
+        modelName = request.form['Model_Name']
+        productName = request.form['Product_Name']
+        serialNumber = request.form['Serial_Number']
+        ProductSoldDate = request.form['ProductSoldDate']
 
         cur = mysql.cursor()
 
-       
+        
         brand_s = 'INSERT INTO Brand(Brand_Name) VALUES(%s);'
-        cur.execute(brand_s, (brand_name,))
+        cur.execute(brand_s, (brandName,))
         mysql.commit()
 
         
         model_s = 'INSERT INTO Model(Model_Name) VALUES(%s);'
-        cur.execute(model_s, (model_name,))
+        cur.execute(model_s, (modelName,))
         mysql.commit()
 
-    
-        product_s = 'INSERT INTO Product(Product_Name, Serial_Number, Product_Sold_Date) VALUES(%s, %s, %s);'
-        cur.execute(product_s, (product_name, serial_number, product_sold_date))
+        
+        product_s = 'INSERT INTO Product(Product_Name, Serial_Number, ProductSoldDate) VALUES(%s, %s, %s);'
+        cur.execute(product_s, (productName, serialNumber,ProductSoldDate))
         mysql.commit()
 
         cur.close()
+
     else:
         return render_template('add.html')
 
     return '{"Result":"Success"}'
+
 
 @app.route("/", methods=['GET'])
 def hello():
@@ -53,8 +55,9 @@ def hello():
 
     cur = mysql.cursor()
 
+   
     sql_query = '''
-        SELECT Brand.Brand_Name, Model.Model_Name, Product.Product_Name, Product.Serial_Number, Product.Product_Sold_Date
+        SELECT Brand.Brand_Name, Model.Model_Name, Product.Product_Name, Product.Serial_Number,Product.ProductSoldDate
         FROM Product
         JOIN Model ON Product.Model_ID = Model.Model_ID
         JOIN Brand ON Model.Brand_ID = Brand.Brand_ID
@@ -65,17 +68,15 @@ def hello():
     rv = cur.fetchall()
 
     Results = []
-
     for row in rv:
         Result = {}
-        Result['Brand_Name'] = row[0]
+        Result['Brand_Name'] = row[0].replace('\n', ' ')
         Result['Model_Name'] = row[1]
         Result['Product_Name'] = row[2]
         Result['Serial_Number'] = row[3]
-        Result['Product_Sold_Date'] = row[4].isoformat() if row[4] else None
-        warranty_check = (datetime.now() - row[4]).days if row[4] else None
-        Result['WarrantyCheck'] = 'Warranty is still valid.' if warranty_check and warranty_check <= 730 else 'Warranty has expired.'
+        Result['ProductSoldDate'] = row[4].isoformat() if row[4] else None
         Results.append(Result)
+        
 
     response = {'Results': Results, 'count': len(Results)}
     ret = app.response_class(
@@ -86,5 +87,7 @@ def hello():
 
     return ret
 
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port='8080', debug=True, ssl_context=('/etc/letsencrypt/live/msubuntu.northeurope.cloudapp.azure.com/cert.pem', '/etc/letsencrypt/live/msubuntu.northeurope.cloudapp.azure.com/privkey.pem'))
+    app.run(host='0.0.0.0', port='8080', debug=True, ssl_context=('/etc/letsencrypt/live/msubuntu.northeurope.cloudapp.azure.com/cert.pem', '/etc/letsencrypt/live/msubuntu.northeurope.cloudapp.azure.com/privkey.pem'))   js:<!DOCTYPE html>
+<html lang="en">
