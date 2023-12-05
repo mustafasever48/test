7ab641757967c9f3e0f5a9dc1f206fb736991c6b
mustafasever48 <82729984+mustafasever48@users.noreply.github.com>
2023-12-05 16:10:06 +0000

    return render_template('/var/www/html/technical', technicians=technicians)

--- app.py
+++ app.py
@@ -122,7 +122,7 @@ def technical():
     technicians = cur.fetchall()
     cur.close()
 
-    return render_template('/var/www/html/technical.html', technicians=technicians )
+    return render_template('/var/www/html/technical', technicians=technicians)
 
 
 
