82895f565192a9141f1e19f594afbec9ab3a873f
mustafasever48 <82729984+mustafasever48@users.noreply.github.com>
2023-12-05 11:40:58 +0000

    len(Results)

--- app.py
+++ app.py
@@ -3,11 +3,9 @@ import mysql.connector
 from flask_cors import CORS
 import json
 from datetime import datetime
-from datetime import datetime, timedelta
 
 app = Flask(__name__)
 CORS(app)
-
 
 mysql = mysql.connector.connect(
     user='web',
@@ -25,22 +23,24 @@ def add():
         serial_number = request.form['Serial_Number']
         product_sold_date = request.form['Product_Sold_Date']
 
+        cur = mysql.cursor()
+
+       
+        brand_s = 'INSERT INTO Brand(Brand_Name) VALUES(%s);'
+        cur.execute(brand_s, (brand_name,))
+        mysql.commit()
+
+        
+        model_s = 'INSERT INTO Model(Model_Name) VALUES(%s);'
+        cur.execute(model_s, (model_name,))
+        mysql.commit()
+
     
-        brand_result = db.Brand.insert_one({"Brand_Name": brand_name})
-        brand_id = brand_result.inserted_id
+        product_s = 'INSERT INTO Product(Product_Name, Serial_Number, Product_Sold_Date) VALUES(%s, %s, %s);'
+        cur.execute(product_s, (product_name, serial_number, product_sold_date))
+        mysql.commit()
 
-     
-        model_result = db.Model.insert_one({"Model_Name": model_name, "Brand_ID": brand_id})
-        model_id = model_result.inserted_id
-
-
-        db.Product.insert_one({
-            "Product_Name": product_name,
-            "Serial_Number": serial_number,
-            "Product_Sold_Date": datetime.strptime(product_sold_date, "%Y-%m-%d"),
-            "Model_ID": model_id
-        })
-
+        cur.close()
     else:
         return render_template('add.html')
 
@@ -50,33 +50,33 @@ def add():
 def hello():
     serial_number = request.args.get('serial_number', '')
 
-    results = db.Product.aggregate([
-        {"$match": {"Serial_Number": serial_number}},
-        {"$lookup": {
-            "from": "Model",
-            "localField": "Model_ID",
-            "foreignField": "_id",
-            "as": "model_info"
-        }},
-        {"$unwind": "$model_info"},
-        {"$lookup": {
-            "from": "Brand",
-            "localField": "model_info.Brand_ID",
-            "foreignField": "_id",
-            "as": "brand_info"
-        }},
-        {"$unwind": "$brand_info"},
-        {"$project": {
-            "_id": 0,
-            "Brand_Name": "$brand_info.Brand_Name",
-            "Model_Name": "$model_info.Model_Name",
-            "Product_Name": "$Product_Name",
-            "Serial_Number": "$Serial_Number",
-            "Product_Sold_Date": {"$dateToString": {"format": "%Y-%m-%d", "date": "$Product_Sold_Date"}}
-        }}
-    ])
+    cur = mysql.cursor()
 
-    response = {"Results": list(results), "count": results.length()}
+    sql_query = '''
+        SELECT Brand.Brand_Name, Model.Model_Name, Product.Product_Name, Product.Serial_Number, Product.Product_Sold_Date
+        FROM Product
+        JOIN Model ON Product.Model_ID = Model.Model_ID
+        JOIN Brand ON Model.Brand_ID = Brand.Brand_ID
+        WHERE Product.Serial_Number = %s;
+    '''
+
+    cur.execute(sql_query, (serial_number,))
+    rv = cur.fetchall()
+
+    Results = []
+
+    for row in rv:
+        Result = {}
+        Result['Brand_Name'] = row[0]
+        Result['Model_Name'] = row[1]
+        Result['Product_Name'] = row[2]
+        Result['Serial_Number'] = row[3]
+        Result['Product_Sold_Date'] = row[4].isoformat() if row[4] else None
+        warranty_check = (datetime.now() - row[4]).days if row[4] else None
+        Result['WarrantyCheck'] = 'Warranty is still valid.' if warranty_check and warranty_check <= 730 else 'Warranty has expired.'
+        Results.append(Result)
+
+    response = {'Results': Results, 'count': len(Results)}
     ret = app.response_class(
         response=json.dumps(response),
         status=200,
