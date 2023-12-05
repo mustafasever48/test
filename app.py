cbfe43ae0d404581d6dfbb2980d6f4ff49779387
mustafasever48 <82729984+mustafasever48@users.noreply.github.com>
2023-12-05 11:34:47 +0000

    Result['WarrantyCheck'] = 'Warranty is still valid.' if warranty_check <= 730 else 'Warranty has expired.'

--- app.py
+++ app.py
@@ -1,85 +1,75 @@
 from flask import Flask, render_template, request
+import mysql.connector
 from flask_cors import CORS
-from pymongo import MongoClient
 import json
 from datetime import datetime
-
+from datetime import datetime, timedelta
+mysql = mysql.connector.connect(
+    user='web',
+    password='webPass',
+    host='127.0.0.1',
+    database='rma'
+)
 app = Flask(__name__)
 CORS(app)
-
-
-client = MongoClient('mongodb+srv://<mustafasever7>:<FnCJPXkPUh5GE6uY>@cluster.mongodb.net/<cluster0>')
-db = client['Cluster0']  # MongoDB veritabanı adı
-
 @app.route("/add", methods=['POST'])
 def add():
     if request.method == 'POST':
-        brand_name = request.form['Brand_Name']
-        model_name = request.form['Model_Name']
-        product_name = request.form['Product_Name']
-        serial_number = request.form['Serial_Number']
-        product_sold_date = request.form['Product_Sold_Date']
-
-    
-        brand_result = db.Brand.insert_one({"Brand_Name": brand_name})
-        brand_id = brand_result.inserted_id
-
-     
-        model_result = db.Model.insert_one({"Model_Name": model_name, "Brand_ID": brand_id})
-        model_id = model_result.inserted_id
-
-
-        db.Product.insert_one({
-            "Product_Name": product_name,
-            "Serial_Number": serial_number,
-            "Product_Sold_Date": datetime.strptime(product_sold_date, "%Y-%m-%d"),
-            "Model_ID": model_id
-        })
-
+        brandName = request.form['Brand_Name']
+        modelName = request.form['Model_Name']
+        productName = request.form['Product_Name']
+        serialNumber = request.form['Serial_Number']
+        ProductSoldDate = request.form['ProductSoldDate']
+        cur = mysql.cursor()
+        
+        brand_s = 'INSERT INTO Brand(Brand_Name) VALUES(%s);'
+        cur.execute(brand_s, (brandName,))
+        mysql.commit()
+        
+        model_s = 'INSERT INTO Model(Model_Name) VALUES(%s);'
+        cur.execute(model_s, (modelName,))
+        mysql.commit()
+        
+        product_s = 'INSERT INTO Product(Product_Name, Serial_Number, ProductSoldDate) VALUES(%s, %s, %s);'
+        cur.execute(product_s, (productName, serialNumber,ProductSoldDate))
+        mysql.commit()
+        cur.close()
     else:
         return render_template('add.html')
-
     return '{"Result":"Success"}'
-
 @app.route("/", methods=['GET'])
 def hello():
     serial_number = request.args.get('serial_number', '')
+    cur = mysql.cursor()
+   
+    sql_query = '''
+        SELECT Brand.Brand_Name, Model.Model_Name, Product.Product_Name, Product.Serial_Number,Product.ProductSoldDate
+        FROM Product
+        JOIN Model ON Product.Model_ID = Model.Model_ID
+        JOIN Brand ON Model.Brand_ID = Brand.Brand_ID
+        WHERE Product.Serial_Number = %s;
+    '''
+    cur.execute(sql_query, (serial_number,))
+    rv = cur.fetchall()
+    Results = []
+    for row in rv:
+        Result = {}
+        Result['Brand_Name'] = row[0].replace('\n', ' ')
+        Result['Model_Name'] = row[1]
+        Result['Product_Name'] = row[2]
+        Result['Serial_Number'] = row[3]
+        Result['ProductSoldDate'] = row[4].isoformat() if row[4] else None
+        warranty_check = (datetime.now() - row[4]).days
+        Result['WarrantyCheck'] = 'Warranty is still valid.' if warranty_check <= 730 else 'Warranty has expired.'
+        Results.append(Result)
 
-    # MongoDB sorgusu
-    results = db.Product.aggregate([
-        {"$match": {"Serial_Number": serial_number}},
-        {"$lookup": {
-            "from": "Model",
-            "localField": "Model_ID",
-            "foreignField": "_id",
-            "as": "model_info"
-        }},
-        {"$unwind": "$model_info"},
-        {"$lookup": {
-            "from": "Brand",
-            "localField": "model_info.Brand_ID",
-            "foreignField": "_id",
-            "as": "brand_info"
-        }},
-        {"$unwind": "$brand_info"},
-        {"$project": {
-            "_id": 0,
-            "Brand_Name": "$brand_info.Brand_Name",
-            "Model_Name": "$model_info.Model_Name",
-            "Product_Name": "$Product_Name",
-            "Serial_Number": "$Serial_Number",
-            "Product_Sold_Date": {"$dateToString": {"format": "%Y-%m-%d", "date": "$Product_Sold_Date"}}
-        }}
-    ])
 
-    response = {"Results": list(results), "count": results.length()}
+    response = {'Results': Results, 'count': len(Results)}
     ret = app.response_class(
         response=json.dumps(response),
         status=200,
         mimetype='application/json'
     )
-
     return ret
-
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port='8080', debug=True, ssl_context=('/etc/letsencrypt/live/msubuntu.northeurope.cloudapp.azure.com/cert.pem', '/etc/letsencrypt/live/msubuntu.northeurope.cloudapp.azure.com/privkey.pem'))
