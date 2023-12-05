3db88acbbac021dc6bad1f421a551431d314a624
mustafasever48 <82729984+mustafasever48@users.noreply.github.com>
2023-12-05 14:59:14 +0000

    from flask import Flask, render_template, request, redirect, url_for

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 import mysql.connector
 from flask_cors import CORS
 import json
