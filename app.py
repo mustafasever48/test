d4603faf73def3669d0bc877e6bd35527ae4515c
mustafasever48 <82729984+mustafasever48@users.noreply.github.com>
2023-12-05 21:33:09 +0000

    @app.route("/technical", methods=['GET'])

--- app.py
+++ app.py
@@ -114,52 +114,14 @@ def create_rma():
 
 
 
-@app.route("/", methods=['GET'])
-def hello():
-    serial_number = request.args.get('serial_number', '')
-
+@app.route("/technical", methods=['GET'])
+def technical():
     cur = mysql.cursor()
-    
-    sql_query = '''
-        SELECT RMA.RMA_ID, RMA.Inspaction_Start_Date, RMA.Inspeciton_Completion_Date,
-               RMA.Product_Defect, RMA.Check_Issue, RMA.Result_Issue,
-               RMA.Product_ID, RMA.Technician_ID, RMA.Return_Shipping_ID, RMA.Shipping_Brand_ID,
-               Brand.Brand_Name, Model.Model_Name, Product.Product_Name, Product.Serial_Number, Product.Product_Sold_Date
-        FROM RMA
-        JOIN Product ON RMA.Product_ID = Product.Product_ID
-        JOIN Model ON Product.Model_ID = Model.Model_ID
-        JOIN Brand ON Model.Brand_ID = Brand.Brand_ID
-        WHERE Product.Serial_Number = %s;
-    '''
+    cur.execute('SELECT * FROM RMA;')
+    rma_data = cur.fetchall()
+    cur.close()
 
-    cur.execute(sql_query, (serial_number,))
-    rv = cur.fetchall()
-
-    Results = []
-    for row in rv:
-        Result = {
-            'RMA_ID': row[0],
-            'Inspaction_Start_Date': row[1].isoformat() if row[1] else None,
-            'Inspeciton_Completion_Date': row[2].isoformat() if row[2] else None,
-            'Product_Defect': row[3],
-            'Check_Issue': row[4],
-            'Result_Issue': row[5],
-            'Product_ID': row[6],
-            'Technician_ID': row[7],
-            'Return_Shipping_ID': row[8],
-            'Shipping_Brand_ID': row[9],
-            'Brand_Name': row[10].replace('\n', ' '),
-            'Model_Name': row[11],
-            'Product_Name': row[12],
-            'Serial_Number': row[13],
-            'Product_Sold_Date': row[14].isoformat() if row[14] else None
-        }
-        Results.append(Result)
-
-    response = {'Results': Results, 'count': len(Results)}
-    ret = jsonify(response)
-
-    return ret
+    return jsonify(rma_data)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port='8080', debug=True, ssl_context=('/etc/letsencrypt/live/msubuntu.northeurope.cloudapp.azure.com/cert.pem', '/etc/letsencrypt/live/msubuntu.northeurope.cloudapp.azure.com/privkey.pem'))
