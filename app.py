2d35de299fafc21503cb39eb325442e62992c7bd
mustafasever48 <82729984+mustafasever48@users.noreply.github.com>
2023-12-11 15:17:30 +0000

    return render_template('/var/www/html/templates/')

--- app.py
+++ app.py
@@ -145,7 +145,7 @@ def check_rma_status():
 @app.route('/technical')
 def technical_page():
     
-    return render_template('/var/www/html/templates/technical.html')
+    return render_template('/var/www/html/templates/')
 
 
 
