8fb637cda80c4bb31273e37bc35b0ac9db13ec1a
mustafasever48 <82729984+mustafasever48@users.noreply.github.com>
2023-12-11 18:03:58 +0000

    from logging.config import dictConfig dictConfig({     'version': 1,     'formatters': {'default': {         'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',     }},     'handlers': {'wsgi': {         'class': 'logging.StreamHandler',         'stream': 'ext://flask.logging.wsgi_errors_stream',         'formatter': 'default'     }},     'root': {         'level': 'INFO',         'handlers': ['wsgi']     } })

--- app.py
+++ app.py
@@ -11,7 +11,22 @@ mysql = mysql.connector.connect(
     host='127.0.0.1',
     database='rma'
 )
-
+from logging.config import dictConfig
+dictConfig({
+    'version': 1,
+    'formatters': {'default': {
+        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+    }},
+    'handlers': {'wsgi': {
+        'class': 'logging.StreamHandler',
+        'stream': 'ext://flask.logging.wsgi_errors_stream',
+        'formatter': 'default'
+    }},
+    'root': {
+        'level': 'INFO',
+        'handlers': ['wsgi']
+    }
+})
 app = Flask(__name__)
 CORS(app)
 
