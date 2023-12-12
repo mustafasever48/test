af1d5f46e7ef1d336bfb5f77afa8e414a1ed2870
mustafasever48 <82729984+mustafasever48@users.noreply.github.com>
2023-12-12 21:15:20 +0000

    @app.route('/technical/rma_details.html, methods=['POST'])

--- app.py
+++ app.py
@@ -249,7 +249,7 @@ def get_rma_details():
 
 
 
-@app.route('/update_rma_details', methods=['POST'])
+@app.route('/technical/rma_details.html, methods=['POST'])
 def update_rma_details():
     try:
         rma_id = request.form.get('rma_id')
