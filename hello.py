6ec6d34aa148ba0ab683a309bdda0dd50c25f8ee
mustafasever48 <82729984+mustafasever48@users.noreply.github.com>
2023-12-13 21:11:39 +0000

    deleted file that we did in the class

--- hello.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-app = Flask(__name__)
-@app.route("/")#URL leading to method
-def hello(): # Name of the method
- return("Hello World!") #indent this line
-if __name__ == "__main__":
-  app.run(host='0.0.0.0',port='8080', ssl_context=('/etc/letsencrypt/live/msubuntu.northeurope.cloudapp.azure.com/cert.pem','/etc/letsencrypt/live/msubuntu.northeurope.cloudapp.azure.com/privkey.pem')) #Run the flask app at port 8080
